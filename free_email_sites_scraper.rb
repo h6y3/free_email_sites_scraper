76a9b8ea6268838e22e3f6534e26b0f46fd0f559
Han Yuan <han.yuan@gmail.com>
2020-07-25 16:38:28 -0700

    Added ability to generate a ruby array for people who want to hardcode or generate a txt file

--- free_email_sites_scraper.rb
+++ free_email_sites_scraper.rb
@@ -1,11 +1,15 @@
 require "nokogiri"
 require "open-uri"
 require "pry"
+require "csv"
+require "erb"
 
 class FreeEmailSitesScraper
   SCRAPE_TARGET = "https://knowledge.hubspot.com/forms/what-domains-are-blocked-when-using-the-forms-email-domains-to-block-feature"
   BODY_SELECTOR = ".post-body p"
   PARAGRAPHS_TO_SKIP = 4
+
+  attr_accessor :emails
 
   def initialize
     html = open(SCRAPE_TARGET)
@@ -13,6 +17,22 @@ class FreeEmailSitesScraper
     body = locate_body(doc)
     @emails = scrape_emails(body)
   end
+
+  def to_txt(file)
+    File.open(file, "w") do |f|
+      @emails.each do |email|
+        f.puts email
+      end
+    end
+
+  end
+
+  def to_ruby(file)
+    ruby_code = ERB.new(ruby_template).result(binding)
+    File.open(file, "w") { |f| f.write ruby_code }
+  end
+
+  private 
 
   def locate_body(doc)
     doc.at_css(BODY_SELECTOR)
@@ -31,7 +51,10 @@ class FreeEmailSitesScraper
     cursor = cursor.map {|xml| xml.text}
   end
 
-  def emails
-    @emails
+  def ruby_template
+    %{
+      free_email_providers = ["<%= @emails.compact.reject(&:empty?).join('","') %>"]
+    }
   end
+
 end
---
+++ sample.rb
@@ -0,0 +1,7 @@
+require './free_email_sites_scraper.rb'
+
+scraper = FreeEmailSitesScraper.new
+puts scraper.emails #array of string emails
+
+scraper.to_txt("emails.txt") #write csv file
+scraper.to_ruby("email_declaration.rb") #write an array declaration for hardcoding entries
