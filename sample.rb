c372b6bb03d1f0d15e5b424e518e57651550ee6c
Han Yuan <han.yuan@gmail.com>
2020-07-26 05:42:24 -0700

    Update sample.rb
    Changed comment

--- sample.rb
+++ sample.rb
@@ -3,5 +3,5 @@ require './free_email_sites_scraper.rb'
 scraper = FreeEmailSitesScraper.new
 puts scraper.emails #array of string emails
 
-scraper.to_txt("emails.txt") #write csv file
+scraper.to_txt("emails.txt") #write txt file
 scraper.to_ruby("email_declaration.rb") #write an array declaration for hardcoding entries
